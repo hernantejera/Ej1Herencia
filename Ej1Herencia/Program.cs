b98ed26f4f78045f4e2b49b20cbaf58cb10b8a12
hernantejera <hernanmtejera@gmail.com>
2023-07-15 12:04:03 -0300

    Agregar archivos de proyecto.

---
+++ Ej1Herencia/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ej1Herencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
