99649e700da4ac301c7abaf647f169088244bd84
hernantejera <hernanmtejera@gmail.com>
2023-07-15 13:05:22 -0300

    Tarea resuelta

--- Ej1Herencia/Program.cs
+++ Ej1Herencia/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Ej1Herencia
+﻿using System.Reflection.PortableExecutable;
+
+namespace Ej1Herencia
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Coche coche = new Coche()
+            {
+                CantidadPuertas = 5,
+                Marca = "Renault",
+                Modelo = "Clio",
+            };
+            Motocicleta motocicleta = new Motocicleta()
+            {
+                CantidadPatas = 1,
+                Marca = "Honda",
+                Modelo = "Tornado",
+            };
+            Console.WriteLine($"El coche tiene {coche.CantidadPuertas} " +
+                $"puertas , su marca es {coche.Marca} y su modelo es {coche.Modelo}");
+
+            Console.WriteLine($"La motocicleta tiene {motocicleta.CantidadPatas}" +
+                $" patas , su marca es {motocicleta.Marca} y su modelo es {motocicleta.Modelo}");
         }
     }
 }
